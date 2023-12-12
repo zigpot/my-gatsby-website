c7b62914bbdf16317f5b048ab626b17901d43a5e
Fariz A <fariz.adnan.w@mail.ugm.ac.id>
2023-12-12 14:10:22 +0700

    publishing

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,7 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: "/my-gatsby-site",
   siteMetadata: {
     title: `fariz' site`,
     siteUrl: `https://www.yourdomain.tld`
